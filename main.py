613eb240a11cc26b48f16d1305abb580974c5e97
tonystarkss <73152142+tonystarkss@users.noreply.github.com>
2020-11-05 11:53:41 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+adjective1 = input("Enter an adjective: ")
+name = input("Enter a name for the chef: ")
+noun1 = input("Enter a Noun: ")
+adjective2 = input("Enter a second adjective: ")
+noun2 = input("Enter a second noun: ")
+adjective3 = input("Enter a third adjective: ")
+adjective4 = input("Enter a fourth adjective: ")
+plural_noun = input("Enter a plural noun: ")
+noun3 = input("Enter a third noun: ")
+number1 = input("Enter a number: ")
+shapes = input("Enter a shape: ")
+food1 = input("Enter a food item: ")
+food2 = input("Enter a second food item: ")
+number2 = input("Enter in a second number: ")
+# Variables for the mad libs inputs
+
+print("Pizza was invented by a " + adjective1 + " chef named " + name + ".")
+print("To make a pizza, you need to take a lump of " + noun1 + " and make a thin, round " + adjective2 + "" + noun2)
+print("Then you cover it with " + adjective3 + " sauce, " + adjective4 + " cheese, and fresh chopped " + plural_noun)
+print("Next you have to bake it in a very hot " + noun3 + ".")
+print("When it is done, cut it into " + number1 + "" + shapes + ".")
+print("Some kids like " + food1 + " pizza the best, but my favourite is the " + food2 + " pizza.")
+print("If I could, I would eat pizza " + number2 + " times a day!")
+# Sentences with variables
+
+
+
